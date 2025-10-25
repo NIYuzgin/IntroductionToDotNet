ddda3a752b114cc81781fec2ec7390fc1fcf7ee3
Николай Немов <prost91@outlook.com>
2025-10-25 20:23:51 +0300

    Geometry DONE

---
+++ DataTypes/Program.cs
@@ -0,0 +1,122 @@
+﻿//#define DATATYPES
+//#define CONSTANTS
+#define TYPE_CONVERSIONS
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		const string delimiter = "\n-----------------------------\n";
+		static void Main(string[] args)
+		{
+#if DATATYPES
+			Console.WriteLine($"bool занимает {sizeof(bool)} байт памяти, класс обвертка Boolean;");
+			Console.WriteLine(bool.FalseString);
+			Console.WriteLine(bool.TrueString);
+			Console.WriteLine(typeof(bool));
+
+			Console.WriteLine(delimiter);
+
+			//////////////////////////////////////////////////////////
+
+			Console.WriteLine
+				(
+$@"Тип данных char занимает {sizeof(char)} Байт памяти, и принимает значения
+в диапазоне от {(int)char.MinValue} до {(int)char.MaxValue}. Класс-обвертка - {typeof(char)}"
+				);
+
+			Console.WriteLine(delimiter);
+
+			// @ - RAW-строка - игнорирует все специальные символы и Esc - последовательности
+
+			Console.WriteLine
+				(
+				@"RAW-строка - игнорирует все специальные символы и Esc - последовательности \t\n,
+				т.е. воспринимается как есть 'as-is'"
+				);
+
+			// ushort, uint, ulong
+			// sbyte (Signed Byte)
+			// sbyte	byte		1 Байт
+			// short	ushort		2 Байт
+			// int		uint		4 Байт
+			// long		ulong		8 Байт
+
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine
+				(
+$@"byte занимает {sizeof(byte)} байт памяти,
+и принимает значения в диапазоне от {byte.MinValue} до {byte.MaxValue},
+класс обвертка {typeof(byte)}"
+				);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine
+				(
+$@"sbyte занимает {sizeof(sbyte)} байт памяти,
+и принимает значения в диапазоне от {sbyte.MinValue} до {sbyte.MaxValue},
+класс обвертка {typeof(sbyte)}"
+				);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine
+				(
+				$@"float занимает {sizeof(float)} байт памяти"
+				);
+			Console.WriteLine(delimiter); 
+			
+			Console.WriteLine
+				(
+				$@"decimal занимает {sizeof(decimal)} байт памяти"
+				);
+			decimal a = 1;
+			//double a = 1;
+			a /= 3;
+			Console.WriteLine(a);
+			a *= 3;
+			Console.WriteLine(a);
+
+			Console.WriteLine(delimiter);
+#endif
+
+			//////////////////////////////////////////////////
+
+#if CONSTANTS
+			Console.WriteLine("Hello".GetType());
+			Console.WriteLine(5.GetType());
+			Console.WriteLine(5.0.GetType());
+#endif
+
+#if TYPE_CONVERSIONS
+			int n = 5;
+			while (n-- > 0)
+			{
+				Console.WriteLine(n);
+			}
+
+			Console.WriteLine(delimiter);
+
+			//ushort b = 2;
+			//Console.WriteLine(b); 
+
+			//int a = 2;
+			double a = 2.2;
+			short b = (short)a;
+			Console.WriteLine(b);
+			//bool rain = Convert.ToBoolean("True");
+			bool rain = bool.Parse("false");
+			Console.WriteLine(rain);
+
+#endif
+		}
+	}
+}
---
+++ Geometry/Program.cs
@@ -0,0 +1,183 @@
+﻿//#define GEOMETRY
+#define HARD_CHESS
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry {
+	internal class Program {
+		static void Main(string[] args) {
+
+#if GEOMETRY
+			Console.Write("Введите размер фигуры: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine();
+
+			Console.WriteLine("1)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("2)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j <= i; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("3)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = i; j < n; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("4)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					Console.Write("  ");
+				}
+				for (int j = i; j < n; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("5)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = i; j < n - 1; j++)
+				{
+					Console.Write("  ");
+				}
+				for (int j = n - i - 1; j < n; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("6)");
+			Console.WriteLine();
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n - i; j++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write("/");
+
+				for (int j = 0; j < 2 * i; j++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write("\\");
+				Console.WriteLine();
+			}
+			for (int i = n; i > 0; i--)
+			{
+				for (int j = 0; j < n - i; j++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write(" \\");
+
+				for (int j = 0; j < 2 * (i - 1); j++)
+				{
+					Console.Write(" ");
+				}
+				Console.Write("/");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("7)");
+			Console.WriteLine();
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					if (i % 2 == j % 2)
+					{
+						Console.Write("+ ");
+					}
+					else
+					{
+						Console.Write("- ");
+					}
+				}
+				Console.WriteLine();
+			} 
+#endif
+
+#if HARD_CHESS
+
+			Console.Write("Введите размер доски: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine();
+
+			for (int m = 0; m < n; m++) 
+			{
+				for (int i = 0; i < n; i++) 
+				{
+					for (int j = 0; j < n; j++) 
+					{
+						if (m % 2 == j % 2) 
+						{
+							for (int k = 0; k < n; k++) 
+							{
+								Console.Write("* ");
+							}
+						}
+						else 
+						{
+							for (int k = 0; k < n; k++) 
+							{
+								Console.Write("  ");
+							}
+						}
+					}
+					Console.WriteLine();
+				}
+			}
+#endif
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
