adad835e8663f38f95a8381956d162d31052e7a1
Николай Немов <prost91@outlook.com>
2025-10-26 09:40:03 +0300

    Calc in progress

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -10,6 +10,28 @@ namespace Calc
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("Введите арифметическое выражение вида '2 + 3': ");
+			string expression = Console.ReadLine();
+			expression = expression.Replace('.', ',');
+
+			char[] delimiters = new char[] { '+', '-', '*', '/' };
+			string[] numbers = expression.Split(delimiters);
+
+			Console.WriteLine(numbers[0]);
+			Console.WriteLine(numbers[1]);
+			Console.WriteLine(expression[expression.IndexOfAny(delimiters)]);
+			
+			double a = double.Parse(numbers[0]);
+			double b = double.Parse(numbers[1]);
+			switch (expression[expression.IndexOfAny(delimiters)])
+			{
+				case '+': Console.WriteLine($"{numbers[0]} + {numbers[1]} = {a + b}"); break;
+				case '-': Console.WriteLine($"{numbers[0]} - {numbers[1]} = {a - b}"); break;
+				case '*': Console.WriteLine($"{numbers[0]} * {numbers[1]} = {a * b}"); break;
+				case '/': Console.WriteLine($"{numbers[0]} / {numbers[1]} = {a / b}"); break;
+
+				default: Console.WriteLine("Error: No operation"); break;
+			}
 		}
 	}
 }
